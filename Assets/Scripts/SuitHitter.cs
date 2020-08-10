651cbbfe2fe0c9fb9c3b27d4b5f49545b080c8b2
MakeshiftH2 <39418044+MakeshiftH2@users.noreply.github.com>
2020-08-09 20:23:41 -0700

    New stuff
    Some more WIP also moving so gonna be a while till i get back to working on this

---
+++ Assets/Scripts/SuitHitter.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SuitHitter : MonoBehaviour
+{
+    public Animator suitAnimator;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        suitAnimator = GetComponent<Animator>();
+    }
+
+
+    public void SuitActivator()
+    {
+        suitAnimator.SetBool("onTriggerHit", true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(suitAnimator == null)
+        {
+            return;
+        }
+    }
+}
